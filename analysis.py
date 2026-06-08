28da48a42a3e31c27d6f1fa9fea6d4f704730485
Victoria <torimail13@gmail.com>
2026-06-08 12:28:51 +0200

    Revert "Smth"

--- analysis.py
+++ analysis.py
@@ -6,7 +6,3 @@ print(df)
 average_sales = df["sales"].mean()
 print("Середнє значення:", average_sales)
 print("Це середній рівень продажів по трьох міста")
-#опять тестим что-то
-print("Why you don't commit?")
-#b tot rj
-print("How to deal with i?")
